f58e9f6be9ad6de92bde11b6122729f9204e3c44
João Lucas <joaolucassm0704@gmail.com>
2023-06-25 16:51:05 -0300

    Obtendo um usuário por id

--- src/main/java/com/joao/mongo/services/UserService.java
+++ src/main/java/com/joao/mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.joao.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joao.mongo.domain.User;
 import com.joao.mongo.repository.UserRepository;
+import com.joao.mongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
